6dcd33ef32781c2a6a8ae3ded430915bfc2ee0f8
csiblev <gwapongbobo0919@gmail.com>
2023-02-26 16:45:55 +0800

    passed part4 first and second exercise

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,8 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account obj = new Account("Nonoy", 100.0);
+        obj.deposit(20.0);
+        System.out.println(obj);
     }
 }
--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,11 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account Matthew = new Account("Matthews account", 1000.0);
+        Account Me = new Account("My account", 0);
+        Matthew.withdrawal(100.0);
+        Me.deposit(100.0);
+        System.out.println(Matthew);
+        System.out.println(Me);
     }
 }
